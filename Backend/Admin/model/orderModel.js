9c28df1e68518ba6266a907ee7a1819a3294247a
Megha-Gowda14 <37587561+Megha-Gowda14@users.noreply.github.com>
2021-06-05 01:00:05 +0530

    changes

--- Backend/Admin/model/orderModel.js
+++ Backend/Admin/model/orderModel.js
@@ -3,42 +3,22 @@ const mongoose = require("mongoose");
 
 //order schema
 const orderSchema = mongoose.Schema({
-  customerId: {
-     type: String
-     },
-  customerName: {
-     type: String
-     },
-  carName: {
-     type: String
-     },
-  carNumber: {
-     type: String
-    
-    },
-  custAddress: { 
-    type: String,
-     max: 40 
-    },
-  serviceName: { 
-    type: String
-   },
-  servicePrice: {
-     type: Number 
-    },
-  washerId: { 
-    type: String
-   },
+  customerId: { type: String },
+  customerName: {type: String },            
+  carName: { type: String},        
+  carNumber: { type: String },        
+  custAddress: { type: String,
+                  max: 40 
+               },          
+  serviceName: { type: String},         
+  servicePrice: { type: Number},          
+  washerId: { type: String },     
   requestedOn: { 
-    type: Date,
-     default: Date.now() 
-    },
-  deliveredOn: { 
-    type: Date
-   },
-  status: {
-    type: String,
-  },
+                 type: Date,
+                 default: Date.now() 
+              },
+  deliveredOn: { type: Date},             
+  status: {type: String },       
 });
 
 //export model
