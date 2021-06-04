3608c7b24e00f0d8edb18437f9cba1775222c007
Megha-Gowda14 <37587561+Megha-Gowda14@users.noreply.github.com>
2021-06-04 17:20:26 +0530

    commented

--- Backend/Washer/services/orderServices.js
+++ Backend/Washer/services/orderServices.js
@@ -5,19 +5,19 @@ const OrderController = require("../controllers/orderController");
 
 router.patch(
   "/updateOrder/:orderId",
-  [checkAuth.verifyToken, checkAuth.isMechanic],
+  //[checkAuth.verifyToken, checkAuth.isMechanic],
   OrderController.updateOrder
 );
 
 router.get(
-  "/findInProcessOrders/:mechId",
-  [checkAuth.verifyToken, checkAuth.isMechanic],
+  "/findInProcessOrders/:washId",
+ // [checkAuth.verifyToken, checkAuth.isMechanic],
   OrderController.findInProcessOrders
 );
 
 router.get(
-  "/findMyOrders/:mechId",
-  [checkAuth.verifyToken, checkAuth.isMechanic],
+  "/findMyOrders/:washId",
+ // [checkAuth.verifyToken, checkAuth.isMechanic],
   OrderController.findMyOrders
 );
 
