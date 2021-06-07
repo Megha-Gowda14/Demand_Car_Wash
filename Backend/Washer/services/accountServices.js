81252bc3af78d39a2b9ba74b6be454c5fbc4720a
Megha-Gowda14 <37587561+Megha-Gowda14@users.noreply.github.com>
2021-06-07 20:47:25 +0530

    washer swagger added

--- Backend/Washer/services/accountServices.js
+++ Backend/Washer/services/accountServices.js
@@ -3,12 +3,74 @@ const router = express.Router();
 //const checkAuth = require("../middlewares/check-auth");
 const AccountController = require("../controllers/accountController");
 
+
+/**
+ * @swagger
+ * /washer/account/updateProfile/:washId:
+ *   put:
+ *     summary: update the profile
+ *     description: update washer details
+ *     requestBody: 
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties: 
+ *               id: 
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password: 
+ *                 type: number
+ *               role:
+ *                 type: String 
+ *     responses:
+ *       "200":
+ *         description: profile updated Successfully
+ *         content:
+ *           application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type:string
+ * 
+ */
 router.put(
   "/update/:washId",
   //[checkAuth.verifyToken],
   AccountController.updateProfile
 );
 
+
+/**
+ * @swagger
+ * /washer/account/deleteAccount/:washId:
+ *   delete:
+ *     summary: delete the washer profile
+ *     description: delete the washer profile
+ *     requestBody: 
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties: 
+ *               id: 
+ *                 type: string
+ *     responses:
+ *       "200":
+ *         description: Profile deleted Successfully
+ *         content:
+ *           application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type:string 
+ *  
+ */
 router.delete(
   "/delete/:washId",
   //[checkAuth.verifyToken],
--- Backend/Washer/services/authServices.js
+++ Backend/Washer/services/authServices.js
@@ -18,8 +18,100 @@ router.get(
 );  
 */
 
+/**
+ * @swagger
+ * /washer/auth/signup:
+ *   post:
+ *     summary: washer Signup
+ *     requestBody:
+ *       required: true
+ *       content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   description: name
+ *                 email:
+ *                   type: string
+ *                   description: email
+ *                   example: test@test.com
+ *                 password:
+ *                   type: string
+ *                   description: password
+ *                 mobile:
+ *                   type: string
+ *                   description: number
+ *                 role:
+ *                   type: string
+ *                 status:
+ *                   type: string
+ *     responses:
+ *       "200":
+ *         description: Registration Successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type:string
+ *                 usedId:
+ *                   type:string
+ *                 name:  
+ *                   type:string
+ *                 email:
+ *                   type:string
+ *                 role:  
+ *                   type:string
+ *                 token:
+ *                   type:string
+ */
+
 //signup/login
 router.post("/signup", UsersController.signup);
+
+/**
+ * @swagger
+ * /washer/auth/login:
+ *   post:
+ *     summary: washer Login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                   description: email
+ *                   example: test@test.com
+ *                 password:
+ *                   type: string
+ *                   description: password
+ *     responses:
+ *       "200":
+ *         description: Authentication Successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type:string
+ *                 usedId:
+ *                   type:string
+ *                 name:  
+ *                   type:string
+ *                 email:
+ *                   type:string
+ *                 role:  
+ *                   type:string
+ *                 token:
+ *                   type:string
+ */
 
 router.post("/login", UsersController.login);
 
