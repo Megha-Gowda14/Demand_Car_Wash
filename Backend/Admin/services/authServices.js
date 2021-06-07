37a8b62b6d4f18fd2681208f2db060e60b316cc3
Megha-Gowda14 <37587561+Megha-Gowda14@users.noreply.github.com>
2021-06-07 18:34:13 +0530

    Admin-Auth Swagger

--- Backend/Admin/services/authServices.js
+++ Backend/Admin/services/authServices.js
@@ -3,9 +3,100 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/authController");
 
-//login
+/**
+ * @swagger
+ * /admin/auth/login:
+ *   post:
+ *     summary: AdminLogin
+ *     requestBody:
+ *       required: true
+ *       content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                   description: email
+ *                   example: test@test.com
+ *                 password:
+ *                   type: string
+ *                   description: password
+ *     responses:
+ *       "200":
+ *         description: Authentication Successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type:string
+ *                 usedId:
+ *                   type:string
+ *                 name:  
+ *                   type:string
+ *                 email:
+ *                   type:string
+ *                 role:  
+ *                   type:string
+ *                 token:
+ *                   type:string
+ */
+
+//admin login
 router.post('/login', AuthController.login);
 
+/**
+ * @swagger
+ * /admin/auth/signup:
+ *   post:
+ *     summary: Member Signup
+ *     requestBody:
+ *       required: true
+ *       content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   description: name
+ *                 email:
+ *                   type: string
+ *                   description: email
+ *                   example: test@test.com
+ *                 password:
+ *                   type: string
+ *                   description: password
+ *                 mobile:
+ *                   type: string
+ *                   description: number
+ *                 role:
+ *                   type: string
+ *                 status:
+ *                   type: string
+ *     responses:
+ *       "200":
+ *         description: REgistration Successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type:string
+ *                 usedId:
+ *                   type:string
+ *                 name:  
+ *                   type:string
+ *                 email:
+ *                   type:string
+ *                 role:  
+ *                   type:string
+ *                 token:
+ *                   type:string
+ */
 //signup
 router.post('/signup', AuthController.signup);
 
